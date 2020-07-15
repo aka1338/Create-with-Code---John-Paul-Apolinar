1cee8f870a755dca1096bc9bad59757172d038b2
John Paul Apolinar <john.apolinar@gmail.com>
2020-07-15 01:04:34 -0700

    Added Prototype 1 (Driving game)
    WIP of Driving Game #1 as part of the Create with Code Summer Course.

---
+++ Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// It is in good practice to have zero hard coded numbers. 
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player; 
+    private Vector3 offset = new Vector3(0, 4, -8); 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Offset the camera behind the player
+        transform.position = player.transform.position + offset; 
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Declaring a float requires f at the end. Public due to usage outside of class. 
+    public float speed = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Making the vehicle move forward
+        transform.Translate(Vector3.forward*Time.deltaTime * speed);
+    
+    }
+}
