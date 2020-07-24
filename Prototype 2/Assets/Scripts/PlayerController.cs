c757232cc9ae221e9ea0013f44ceda1776c7a57c
John Paul Apolinar <john.apolinar@gmail.com>
2020-07-24 00:08:42 -0700

    Added Prototype 2
    - Added horizontal movement functionality
    - Implemented player boundaries
    - Still working on getting it to fire meat

---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xBoundary = 10.0f;
+
+    public GameObject projectilePrefab; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Keeps the player inbounds (left) 
+        if (transform.position.x < -xBoundary){
+            transform.position = new Vector3(-xBoundary, transform.position.y, transform.position.z);
+        }
+        // Keeps the player inbounds (right)
+        if (transform.position.x > xBoundary)
+        {
+            transform.position = new Vector3(xBoundary, transform.position.y, transform.position.z);
+        }
+        horizontalInput = Input.GetAxis("Horizontal");
+            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+    }
+}
