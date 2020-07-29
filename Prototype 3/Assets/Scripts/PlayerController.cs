fcf68d24056966c3f7fd956f8c2446363cd29284
John Paul Apolinar <john.apolinar@gmail.com>
2020-07-28 22:58:16 -0700

    Added Prototype 3

---
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier = 10;
+    public bool isOnGround = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true) {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false; 
+        }
+    }
+
+    // Any collision the player makes with any gameObject lets them jump again. 
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true; 
+    }
+}
