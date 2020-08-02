0f98f6ba25cdb1aa939baa5d2e124835f8204d45
John Paul Apolinar <john.apolinar@gmail.com>
2020-08-02 16:38:08 -0700

    Completed Prototype 3
    - Changed the color of the road
    - Added seamless scrolling background
    - Added player animations
    - Added player particles
    - Added sound!

--- Prototype 3/Assets/Scripts/MoveLeft.cs
+++ Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 30; 
+    private float speed = 30;
+    private PlayerController playerControllerScript;
+    private float leftBound = -15;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Sets the PlayerController script to the actual player in the scene. 
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed); 
+        // Keeps everything moving until GameOver is switched to true. 
+        if (playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Assets/Scripts/PlayerController.cs
@@ -5,29 +5,56 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
+    private Animator playerAnim;
+    private AudioSource playerAudio; 
+    public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
+    public AudioClip jumpSound;
+    public AudioClip crashSound; 
+
     public float jumpForce = 10;
     public float gravityModifier = 10;
-    public bool isOnGround = true; 
+    public bool isOnGround = true;
+    public bool gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
-        Physics.gravity *= gravityModifier; 
+        playerAnim = GetComponent<Animator>(); 
+        Physics.gravity *= gravityModifier;
+        playerAudio = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true) {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround == true && !gameOver) {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false; 
+            playerAudio.PlayOneShot(jumpSound, 0.4f); 
+            isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop(); 
         }
     }
 
     // Any collision the player makes with any gameObject lets them jump again. 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true; 
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            dirtParticle.Play();
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle")) {
+            Debug.Log("Game Over!");
+            playerAudio.PlayOneShot(crashSound, 0.4f);
+            gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+           
+            explosionParticle.Play();
+            dirtParticle.Stop();
+        }
     }
 }
